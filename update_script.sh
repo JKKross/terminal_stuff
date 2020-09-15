1832f7ef36832cdd3ab706b5bf5d35de720b7f59
JKKross <zawadski.jkk@gmail.com>
2020-09-15 20:23:17 +0200

    Update

--- update_script.sh
+++ update_script.sh
@@ -1,9 +1,3 @@
-echo ABOUT TO RUN 'rustup update'
-echo
-rustup update
-echo
-echo 'rustup update' FINISHED
-
 echo ABOUT TO RUN 'brew update'
 echo
 brew update
@@ -24,11 +18,3 @@ cd ~/Programming/foss/swift/
 git pull
 echo
 echo 'git pull' in ~/Programming/foss/swift FINISHED
-
-echo
-echo ABOUT TO RUN 'git pull' in ~/Programming/foss/rust
-echo
-cd ~/Programming/foss/rust/
-git pull
-echo
-echo 'git pull' in ~/Programming/foss/rust FINISHED
